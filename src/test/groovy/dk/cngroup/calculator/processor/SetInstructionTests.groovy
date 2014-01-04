6958323400091667c39702c92df70371746990fc
Tom Metz <metz@cngroup.dk>
2014-01-04 21:06:40 +0100

    Instruction "set" implemented
    - spock unit tests implemented

---
+++ src/test/groovy/dk/cngroup/calculator/processor/SetInstructionTests.groovy
@@ -0,0 +1,42 @@
+package dk.cngroup.calculator.processor
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SetInstructionTests extends Specification {
+
+    private Instruction instruction
+
+    void setup() {
+        instruction = new SetInstruction()
+    }
+
+    @Unroll
+    void 'Single operand computation of "set" to #register an #operand results #result'(int register, int operand, int result) {
+        expect:
+        instruction.execute(register, operand) == result
+
+        where:
+        register | operand | result
+         1 |  1 |  1
+         0 |  1 |  1
+        -2 |  1 |  1
+        -2 | -3 | -3
+         3 |  0 |  0
+         0 |  0 |  0
+        -1 |  4 |  4
+    }
+
+    @Unroll
+    void 'Double operand computation of "set" to #register an #operand1 and #operand2 results #result'(int register, int operand1, int operand2, int result) {
+        expect:
+        instruction.execute(register, operand1, operand2) == result
+
+        where:
+        register | operand1 | operand2 | result
+         1 |  2 |  3 |  3
+        -1 |  2 | -3 | -3
+         0 |  0 |  0 |  0
+    }
+
+}
